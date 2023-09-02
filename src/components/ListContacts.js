b6a70c0564560a2816849f47d60e266af11370f9
Hsu-Susan <hsuhsuthetzaw.harumi@gmail.com>
2023-09-02 15:55:41 +0900

    add controlled component form for searching contacts

---
+++ src/components/ListContacts.js
@@ -0,0 +1,58 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const ListContacts = ({ contacts, onDeleteContact }) => {
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (query) => {
+    setQuery(query.trim());
+  };
+
+  const showingContacts =
+    query === ""
+      ? contacts
+      : contacts.filter((c) => c.name.toLowerCase().includes(query));
+
+  return (
+    <div className="list-contacts">
+      <div className="list-contacts-top">
+        <input
+          className="search-contacts"
+          type="text"
+          placeholder="Search Contacts"
+          value={query}
+          onChange={(event) => updateQuery(event.target.value)}
+        />
+      </div>
+      <ol className="contact-list">
+        {showingContacts.map((contact) => {
+          return (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`,
+                }}
+              ></div>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                className="contact-remove"
+                onClick={() => onDeleteContact(contact)}
+              ></button>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+};
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ListContacts;
